af8ecb6e589b4a5e43c0c77124e748aae42f3020
Dave Morse <dcmorse@gmail.com>
2018-10-02 16:38:58 -0500

    wip on star

--- src/Circle.elm
+++ src/Circle.elm
@@ -13,20 +13,52 @@ main =
 
 view =
     collage 500 500
-        (List.range 1 18 |> List.reverse |> List.map circleizer)
+        -- (List.range 1 18 |> List.reverse |> List.map circleizer)
+        star4
         -- [ circle 50 |> filled red,
         --   circle 40 |> filled white,
         --   circle 30 |> filled blue,
         --   circle 20 |> filled green,
-        --   circle 10 |> filled yellow
+        --   circle 10 |> filled yellow,
+        --   text "Hello" |> size 20 |> filled green |> move (-30,30)
         -- ]
 
-circleizer : Int -> Shape a
-circleizer i =
-     move (toFloat i * 20 - 200, (toFloat (i-9))^2*5 - 100) <| filled (rainbow_pick i) <| circle <| toFloat i * 10
+star4 =
+    [ isosceles2 40 100 |> filled red |> rotate (degrees 0)
+    , isosceles2 40 100 |> filled red |> rotate (degrees 72)
+    , isosceles2 40 100 |> filled red |> rotate (degrees 72*2)
+    , isosceles2 40 100 |> filled red |> rotate (degrees 72*3)
+    , isosceles2 40 100 |> filled red |> rotate (degrees 72*4)
+    ]
+
+isosceles2 base height =
+    polygon [(0,height), (base/2,0), ((0-1) * base / 2,0)]
+
+
+star3 =
+    List.map triangleAtAngle (List.range 1 5)
+
+triangleAtAngle angle =
+    isosceles 40 100 |> filled (rainbow_pick angle) |> rotate (toFloat angle * (degrees 72))
+
+
+redCircle radius =
+    circle radius |> filled red
+
+star =
+    ngon 10 100.0 |> filled red
+
+star2 =
+    polygon [(0,100),
+                 (40 * sin (0.5*2*pi/5),40*cos (0.5*2*pi/5)),
+                 (100 * sin (1*2*pi/5),100*cos (1*2*pi/5)),
+                 (100*sin (2*2*pi/5),100*cos (2*2*pi/5)),
+                 (100*sin (3*2*pi/5),100*cos (3*2*pi/5)),
+                 (100*sin (4*2*pi/5),100*cos (4*2*pi/5))
+            ] |> filled red
 
 rainbow_pick i =
-    case ((i-1)%6) of
+    case i of
         0 -> red
         1 -> orange
         2 -> yellow
